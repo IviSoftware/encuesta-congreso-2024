60c69bc381d3ad014b3e7418159196de3d30a445
iviDev <iva_men_ase@hotmail.com>
2024-09-22 13:38:41 -0600

    suportBtn

--- src/components/atomos/SupportButton/index.jsx
+++ src/components/atomos/SupportButton/index.jsx
@@ -12,11 +12,18 @@ function SupportButton() {
   const supportStartTime = 9; // 09:00 am
   const supportEndTime = 17.67; // 5:40 pm (5 + 40/60)
 
-  // Función para verificar si está en horario de soporte
+  // Función para verificar si está en horario y día de soporte
   const isSupportAvailable = () => {
     const now = new Date();
+    const currentDay = now.getDay(); // 0 = domingo, 1 = lunes, ..., 6 = sábado
     const currentHour = now.getHours() + now.getMinutes() / 60; // Hora actual con minutos como decimales
-    return currentHour >= supportStartTime && currentHour <= supportEndTime;
+
+    // Verificar si es lunes a viernes y está en el horario de soporte
+    const isWeekday = currentDay >= 1 && currentDay <= 5; // 1 a 5 = lunes a viernes
+    const isWithinSupportHours =
+      currentHour >= supportStartTime && currentHour <= supportEndTime;
+
+    return isWeekday && isWithinSupportHours;
   };
 
   // Función para manejar el click del botón de soporte
@@ -26,7 +33,9 @@ function SupportButton() {
         phoneNumbers[Math.floor(Math.random() * phoneNumbers.length)];
       window.open(`https://wa.me/${randomNumber}`, "_blank");
     } else {
-      Swal.fire("El soporte solo está disponible de 09:00 am a 05:40 pm.");
+      Swal.fire(
+        "El soporte solo está disponible de lunes a viernes de 09:00 am a 05:40 pm."
+      );
     }
   };
 
